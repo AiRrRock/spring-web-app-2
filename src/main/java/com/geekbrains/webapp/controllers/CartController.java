cac28e6c8f560705dad71efd00e44e3e49c6c1ac
Airt <alex.borichev@gmail.com>
2021-09-14 12:55:08 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/webapp/controllers/CartController.java
@@ -0,0 +1,35 @@
+package com.geekbrains.webapp.controllers;
+
+import com.geekbrains.webapp.services.CartService;
+import com.geekbrains.webapp.utils.Cart;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+
+    @GetMapping
+    public Cart getCartForCurrentUser() {
+        return cartService.getCartForCurrentUser();
+    }
+
+    @GetMapping("/add/{productId}")
+    public void addToCart(@PathVariable Long productId) {
+        cartService.addItem(productId);
+    }
+
+    @GetMapping("/decrement/{productId}")
+    public void decrementItem(@PathVariable Long productId) {
+        cartService.decrementItem(productId);
+    }
+
+    @GetMapping("/remove/{productId}")
+    public void removeItem(@PathVariable Long productId) {
+        cartService.removeItem(productId);
+    }
+}
---
+++ src/main/java/com/geekbrains/webapp/services/CartService.java
@@ -0,0 +1,45 @@
+package com.geekbrains.webapp.services;
+
+import com.geekbrains.webapp.exceptions.ResourceNotFoundException;
+import com.geekbrains.webapp.model.Product;
+import com.geekbrains.webapp.utils.Cart;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final ProductService productService;
+    private Cart cart;
+
+    @PostConstruct
+    public void init() {
+        this.cart = new Cart();
+    }
+
+    public Cart getCartForCurrentUser() {
+        return cart;
+    }
+
+    public void addItem(Long productId) {
+        if (cart.add(productId)) {
+            return;
+        }
+        Product product = productService.findById(productId).orElseThrow(() -> new ResourceNotFoundException("Невозможно добавить продукт в корзину, так как продукт с id: " + productId + " не существует"));
+        cart.add(product);
+    }
+
+    public void removeItem(Long productId) {
+        cart.remove(productId);
+    }
+
+    public void decrementItem(Long productId) {
+        cart.decrement(productId);
+    }
+
+    public void clearCart() {
+        cart.clear();
+    }
+}
